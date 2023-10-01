b32ef115766d5363ec0bba937d00c816f64f7368
Derek Lu <derekdylu@gmail.com>
2023-10-02 02:22:38 +0800

    add layout to pubs and projects

--- frontend/src/Containers/Projects/Projects.js
+++ frontend/src/Containers/Projects/Projects.js
@@ -1,8 +1,68 @@
 import React from 'react'
 
+import GitHubIcon from '@mui/icons-material/GitHub';
+import PublicIcon from '@mui/icons-material/Public';
+
+const projs = [
+  {
+    type: "research",
+    title: "Mamamia",
+    year: "2021",
+    abstract: "Lorem Ipsum is Lorem Ipsum",
+    roles: ["Developer", "Coordinator"],
+    awards: ["Developer Award"],
+    images: ["https://via.placeholder.com/150"],
+    links: [
+      {
+        type: "github",
+        url: "https://github.com",
+      },
+      {
+        type: "website",
+        url: "https://example.com",
+      },
+    ],
+  }
+]
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      <div className="flex flex-col mx-auto pt-6 md:pt-12 px-6 md:px-24 lg:px-36 xl:px-48">
+      {
+        projs.map((proj, i) => {
+          return (
+            <div key={i} className="flex flex-col w-full md:flex-row gap-4 md:gap-12">
+              <img src={proj.images[0]} alt={proj.title + "img"} className="h-full"/>
+              <div className="flex flex-col h-full">
+                <p>{proj.type} | {proj.year}</p>
+                <p>{proj.title}</p>
+                <p>{proj.roles.join(", ")} | {proj.awards.join(", ")}</p>
+                <p>{proj.abstract}</p>
+                <div className="flex flex-row gap-4">
+                  {
+                    proj.links.map((link, j) => {
+                      return (
+                        <a key={j} href={link.url} target="_blank" rel="noreferrer">
+                          {
+                            link.type === "github" ? (
+                              <GitHubIcon sx={{ color: "grey" }}/>
+                            ) : (
+                              <PublicIcon sx={{ color: "grey" }}/>
+                            )
+                          }
+                        </a>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Containers/Publications/Publications.js
+++ frontend/src/Containers/Publications/Publications.js
@@ -1,8 +1,39 @@
 import React from 'react'
 
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+const pubs = [
+  {
+    display: "De-Yuan Lu, Lung-Peng Cheng. 2024. (Under Anonymous Review). Submitted to the 2024 CHI Conference on Human Factors in Computing Systems (CHI '24).",
+    type: "conference",
+    series: "CHI '24",
+    authors: ["De-Yuan Lu", "Lung-Peng Cheng"],
+    title: "(Under Anonymous Review)",
+    year: "2024",
+    isbn: "",
+    url: "",
+    doi: "",
+    abstract: "",
+  }
+]
+
 const Publications = () => {
   return (
-    <div>Publications</div>
+    <div>
+      <div className="flex flex-col mx-auto pt-6 md:pt-12 px-6 md:px-24 lg:px-36 xl:px-48">
+      {
+        pubs.map((pub, i) => {
+          return (
+            <div key={i} className="flex flex-row w-full items-center gap-x-4 hover:text-[#1a659e]" style={{ cursor: "pointer" }}>
+              <h1 className="font-bold text-xl">{i+1}</h1>
+              <p>{pub.display}</p>
+              <ArrowForwardIcon/>
+            </div>
+          )
+        })
+      }
+      </div>
+    </div>
   )
 }
 
